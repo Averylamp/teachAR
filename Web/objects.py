24cd2a3ac483e48386a5ae6b9a57fb17057fc2b4
Moin Nadeem <mnadeem@mit.edu>
2018-07-28 17:02:18 -0700

    server side python files

---
+++ Web/objects.py
@@ -0,0 +1,28 @@
+class Book(object):
+    def __init__(self, bookID, coverURL, chatID, expertID, name, author):
+        self.bookID = bookID
+        self.chatID = chatID
+        self.coverURL = coverURL
+        self.expertID = expertID
+        self.name = name
+        self.author = author
+        self.images = []
+
+    def to_dict(self):
+        return vars(self)
+
+
+class Image(object):
+    def __init__(self, imageID, description, height, width, textbookImageURL, ARImageURLs, links, title, videoURLs):
+        self.imageID = imageID
+        self.description = description
+        self.height = height
+        self.width = width
+        self.textbookImageURL = textbookImageURL
+        self.ARImageURls = ARImageURLs
+        self.links = links
+        self.title = title
+        self.videoURLs = videoURLs
+
+    def to_dict(self):
+        return vars(self)
--- Web/server.py
+++ Web/server.py
@@ -1,6 +1,8 @@
 from tinydb import TinyDB, Query
 from flask import Flask
 from google.cloud import firestore
+from objects import Books, Images
+import objects
 import csv
 
 app = Flask(__name__)
@@ -19,6 +21,15 @@ def get_books():
     for d in docs:
         print(d.to_dict())
 
+def process_image_form(db, bookID, imageID, description, height, width, textbookImageURL, ARImageURLs, links, title, videoURLs):
+    books_ref = db.collection(u"books").document(bookID).collection("images").document(imageID) 
+    image = Image(imageID, description, height, width, textbookImageURL, ARImageURLs, links, title, videoURLs) 
+    return books_ref.set(image.to_dict()) 
+
+def process_books_form(db, bookID, chatID, name, author) 
+    books_ref = db.collection(u"books").document(bookID)
+    book = Book(bookID, chatID, expertID, name, author:
+    return books_ref.set(book.to_dict()) 
+    
 if __name__ == '__main__':
-    get_books()
     app.run()
