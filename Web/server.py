962798861f24cb2856708e883c0c36446b6f88a5
Moin Nadeem <mnadeem@mit.edu>
2018-07-28 23:38:04 -0700

    removing file

--- Web/server.py
+++ Web/server.py
@@ -25,9 +25,9 @@ def content_loader_3():
     all_books = [i.to_dict() for i in books]
     return content_loader_page(db, all_books)
 
-def process_image_form(db, bookID, imageID, description, height, width, textbookImageURL, ARImageURLs, links, title, videoURLs):
+def process_image_form(db, bookID, imageID, description, height, width, targetImageURL, ARImageURLs, links, title, videoURLs):
     books_ref = db.collection(u"books").document(bookID).collection("images").document(imageID)
-    image = Image(imageID, description, height, width, textbookImageURL, ARImageURLs, links, title, videoURLs)
+    image = Image(imageID, description, height, width, targetImageURL, ARImageURLs, links, title, videoURLs)
     return books_ref.set(image.to_dict())
 
 def process_books_form(db, bookID, coverURL, chatID, expertID, name, author):
