31a26b22ac3664514a31f9468f59b6d5f93a7e21
Ethan Weber <eweb0124@gmail.com>
2018-07-28 22:08:50 -0700

    update

--- Web/server.py
+++ Web/server.py
@@ -20,28 +20,10 @@ def get_book_info(book_id):
 
 @app.route("/content_loader", methods=['GET', 'POST'])
 def content_loader():
-    return content_loader_page()
-
-def get_books():
-    """
-        Precondition: `export GOOGLE_APPLICATION_CREDENTIALS='<PATH TO JSON'`
-    """
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    json_key = glob.glob(os.path.join(dir_path, "*.json"))[0]
-    export_command = "export GOOGLE_APPLICATION_CREDENTIALS='{}'".format(json_key)
-    print(export_command)
-    # TODO(ethan): this doesn't seem to work
-    os.system(export_command)
-
     db = firestore.Client()
-    books_ref = db.collection(u"books")
-    docs = books_ref.get()
-    # for d in docs:
-    #     print(d.to_dict())
-    # print(docs)
-    for d in docs:
-        print(d.id)
-        print(d.to_dict)
+    books = db.collection(u"books").get()
+    all_books = [i.to_dict() for i in books]
+    return content_loader_page(db, all_books)
 
 def process_image_form(db, bookID, imageID, description, height, width, textbookImageURL, ARImageURLs, links, title, videoURLs):
     books_ref = db.collection(u"books").document(bookID).collection("images").document(imageID)
