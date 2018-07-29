55bd67fda7604efc9fc46ffc5f3eb12776480406
Ethan Weber <eweb0124@gmail.com>
2018-07-28 17:51:01 -0700

    working on content form

--- Web/server.py
+++ Web/server.py
@@ -1,7 +1,8 @@
-from tinydb import TinyDB, Query
 from flask import Flask
 from google.cloud import firestore
 import csv
+import os
+import glob
 
 app = Flask(__name__)
 
@@ -13,11 +14,21 @@ def get_books():
     """
         Precondition: `export GOOGLE_APPLICATION_CREDENTIALS='<PATH TO JSON'`
     """
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    json_key = glob.glob(os.path.join(dir_path, "*.json"))[0]
+    export_command = "export GOOGLE_APPLICATION_CREDENTIALS='{}'".format(json_key)
+    print(export_command)
+    os.system(export_command)
+
     db = firestore.Client()
     books_ref = db.collection(u"books")
     docs = books_ref.get()
+    # for d in docs:
+    #     print(d.to_dict())
+    # print(docs)
     for d in docs:
-        print(d.to_dict())
+        print(d.id)
+        print(d.to_dict)
 
 if __name__ == '__main__':
     get_books()
