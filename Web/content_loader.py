edc3d3e59b1672fcf80f992184af606a9195336a
Ethan Weber <eweb0124@gmail.com>
2018-08-01 01:22:32 -0700

    working towards class abstraction

--- Web/content_loader.py
+++ Web/content_loader.py
@@ -6,27 +6,50 @@ from pytube import YouTube
 from wikikit import WikiKit
 import os
 
-def process_image_form(db, bookID, imageID, description, height, width, targetImageURL, ARImageURLs, links, title, videoURL):
-    books_ref = db.collection(u"books").document(bookID).collection("images").document(imageID)
-    image = Image(imageID, description, height, width, targetImageURL, ARImageURLs, links, title, videoURL)
+
+def process_image_form(db,
+                       bookID,
+                       imageID,
+                       description,
+                       height,
+                       width,
+                       targetImageURL,
+                       ARImageURLs,
+                       links,
+                       title,
+                       videoURL):
+    books_ref = db.collection(u"books").document(
+        bookID).collection("images").document(imageID)
+    image = Image(imageID, description, height, width,
+                  targetImageURL, ARImageURLs, links, title, videoURL)
     print(image.to_dict())
     return books_ref.set(image.to_dict())
 
-def process_books_form(db, bookID, coverURL, chatID, expertID, name, author):
+
+def process_books_form(db,
+                       bookID,
+                       coverURL,
+                       chatID,
+                       expertID,
+                       name,
+                       author):
     books_ref = db.collection(u"books").document(bookID)
     book = Book(bookID, coverURL, chatID, expertID, name, author)
     print(book.to_dict())
     return books_ref.set(book.to_dict())
 
+
 def get_new_image_id(db, bookID):
     # retuns a string of an integer representing the image
-    images = db.collection(u"books").document(bookID).collection("images").get()
+    images = db.collection(u"books").document(
+        bookID).collection("images").get()
     id_list = [int(i.to_dict()['imageID']) for i in images]
     if id_list == []:
         id = 1
     else:
         id = max(id_list) + 1
     return str(id)
+
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = 'static/images'
@@ -36,9 +59,11 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 URL_PREFIX = "http://35.236.74.206"
 # URL_PREFIX = "http://127.0.0.1:5000"
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def get_list_from_ids(id_name, form):
     # return the list of links from the html ids using the _number format
@@ -56,6 +81,7 @@ def get_list_from_ids(id_name, form):
         else:
             exists = False
     return list
+
 
 def content_loader_page(db, all_books):
 
@@ -87,16 +113,20 @@ def content_loader_page(db, all_books):
 
                 # image_id has to be a string of an int
                 image_id = get_new_image_id(db, book_id)
-                location_to_save = "static/images/book_{}_image_{}.jpg".format(book_id, image_id)
+                location_to_save = "static/images/book_{}_image_{}.jpg".format(
+                    book_id, image_id)
                 file.save(location_to_save)
                 image_url = os.path.join(URL_PREFIX, location_to_save)
 
-                if video_links[0]!="":
+                if video_links[0] != "":
                     ending = "book_{}_image_{}".format(book_id, image_id)
-                    YouTube(video_links[0]).streams.filter(progressive=True, file_extension='mp4').first().download("static/videos/", filename=ending)
-                    video_links[0] = os.path.join(URL_PREFIX, "static/videos/{}.mp4".format(ending))
+                    YouTube(video_links[0]).streams.filter(progressive=True, file_extension='mp4').first(
+                    ).download("static/videos/", filename=ending)
+                    video_links[0] = os.path.join(
+                        URL_PREFIX, "static/videos/{}.mp4".format(ending))
 
-                process_image_form(db,book_id,str(image_id),str(description),float(image_height),float(image_width),str(image_url),str(image_links[0]),str(info_links[0]),image_name,str(video_links[0]))
+                process_image_form(db, book_id, str(image_id), str(description), float(image_height), float(
+                    image_width), str(image_url), str(image_links[0]), str(info_links[0]), image_name, str(video_links[0]))
 
                 # display a notification on the site
                 flash(image_name + ' uploaded.')
